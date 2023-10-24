c9d49f6c452c71b49ba95a003dba3288553b2eb7
shuyu36 <sharon87306@gmail.com>
2023-10-24 16:12:04 +0800

    1024 service.html電腦手機版完成

---
+++ js/fanrong_service.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 1,
+      centeredSlides: false,
+      slidesPerGroupSkip: 1,
+      grabCursor: true,
+      keyboard: {
+        enabled: true,
+      },
+      breakpoints: {
+  
+        1660:{
+          slidesPerView: 4,
+          slidesPerGroup: 1,
+        },
+
+        1266:{
+          slidesPerView: 3,
+          slidesPerGroup: 1,
+        },
+
+        
+        769:{
+          slidesPerView: 2,
+          slidesPerGroup: 1,
+        },
+      },
+      spaceBetween: 20, // 这里定义间距的值
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  });
+  
