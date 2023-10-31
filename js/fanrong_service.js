87c62214ec9d7e00f19d3d560d4939994927d6d3
shuyu36 <sharon87306@gmail.com>
2023-10-31 16:32:45 +0800

    1031修改完成

--- js/fanrong_service.js
+++ js/fanrong_service.js
@@ -1,35 +1,34 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var swiper = new Swiper(".mySwiper", {
-      slidesPerView: 1,
-      centeredSlides: false,
-      slidesPerGroupSkip: 1,
-      grabCursor: true,
-      keyboard: {
-        enabled: true,
-      },
-      breakpoints: {
-  
-        1660:{
-          slidesPerView: 4,
-          slidesPerGroup: 1,
-        },
+  AOS.init();
 
-        1266:{
-          slidesPerView: 3,
-          slidesPerGroup: 1,
-        },
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    centeredSlides: false,
+    slidesPerGroupSkip: 1,
+    grabCursor: true,
+    keyboard: {
+      enabled: true,
+    },
+    breakpoints: {
+      1660: {
+        slidesPerView: 4,
+        slidesPerGroup: 1,
+      },
 
-        
-        769:{
-          slidesPerView: 2,
-          slidesPerGroup: 1,
-        },
+      1266: {
+        slidesPerView: 3,
+        slidesPerGroup: 1,
       },
-      spaceBetween: 20, // 这里定义间距的值
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+
+      769: {
+        slidesPerView: 2,
+        slidesPerGroup: 1,
       },
-    });
+    },
+    spaceBetween: 20, // 这里定义间距的值
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
   });
-  
+});
